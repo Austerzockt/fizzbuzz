3f44a491845ae045108d4a0403d8ced588a6e8d3
Simon Wögerbauer <simonwoegerbauer@outlook.com>
2021-04-17 20:09:38 +0200

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Austerzockt/fizzbuzz
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+func main() {
+	for i := 1; i <= 100; i++ {
+		if (i % 5 == 0 && i % 7 == 0) {
+			fmt.Println("fizzbuzz")
+		} else if (i % 5 == 0) {
+			fmt.Println("fizz")
+		} else if (i % 7 == 0) {
+			fmt.Println("buzz")
+		} else {
+			fmt.Println(i)
+		}
+		
+	}
+}
